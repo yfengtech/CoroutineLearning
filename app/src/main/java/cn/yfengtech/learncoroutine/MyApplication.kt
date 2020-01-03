00a6f0ca16426c43618caead9d5dc5c7efebff60
syf <moonlight920@outlook.com>
2020-01-03 22:41:01 +0800

    Initial commit

---
+++ app/src/main/java/cn/yfengtech/learncoroutine/MyApplication.kt
@@ -0,0 +1,15 @@
+package cn.yfengtech.learncoroutine
+
+import android.app.Application
+
+/**
+ * Created by yf.
+ * @date 2020-01-03
+ */
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        
+    }
+}
